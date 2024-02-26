333ca1d5ee315c6051ae976ea38baf509c520290
Michael Gregory <mr.m.gregory@outlook.com>
2024-02-26 15:18:04 +0000

    ENHANCEMENTS: Models created for username  pass and email

---
+++ src/users/model.js
@@ -0,0 +1,21 @@
+const { Datatypes } = require("sequelize");
+const sequelize = require("../db/connections");
+
+const User = sequelize.define("User", {
+  username: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
